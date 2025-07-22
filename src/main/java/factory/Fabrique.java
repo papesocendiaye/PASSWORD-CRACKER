5ebaf4ee9671242b6818791808a49d0b102db14e
papesocendiaye <133932251+papesocendiaye@users.noreply.github.com>
2025-07-22 22:04:20 +0000

    javadoc Fabrique

--- src/main/java/factory/Fabrique.java
+++ src/main/java/factory/Fabrique.java
@@ -6,10 +6,21 @@ import attaque.AttaqueBruteForce;
 import attaque.AttaqueDictionnaire;
 import cible.Cible;
 import cible.ConnexionEnLigne;
-
+/**
+ * Classe responsable de la création d'attaques et de cibles.
+ * Cette classe utilise le patron de conception Fabrique pour créer des objets
+ * de type Attaque et Cible en fonction des paramètres fournis.
+ */
 public class Fabrique {
 
-    // Crée l'attaque selon le type spécifié (BruteForce ou Dictionnaire)
+    /**
+     * Crée une attaque en fonction du type spécifié.
+     * Cette méthode permet de créer soit une attaque par force brute, soit une attaque par dictionnaire.
+     * 
+     * @param attaqueType Le type d'attaque à créer (brute_force ou dictionnaire).
+     * @return Une instance de la classe d'attaque correspondante.
+     * @throws IllegalArgumentException Si le type d'attaque n'est pas pris en charge.
+     */
     public Attaque createAttaque(String attaqueType) {
         if ("brute_force".equalsIgnoreCase(attaqueType)) {
             return new AttaqueBruteForce();
@@ -20,7 +31,15 @@ public class Fabrique {
         }
     }
 
-    // Crée la cible selon le type spécifié (local ou online)
+    /**
+     * Crée une cible en fonction du type spécifié.
+     * Cette méthode permet de créer soit une cible locale, soit une cible en ligne.
+     * 
+     * @param cibleType Le type de cible à créer (local ou online).
+     * @param login Le login à utiliser pour la cible.
+     * @return Une instance de la classe Cible correspondante.
+     * @throws IllegalArgumentException Si le type de cible n'est pas pris en charge.
+     */
     public Cible createCible(String cibleType, String login) {
         if ("local".equalsIgnoreCase(cibleType)) {
             return new ConnexionEnLocale(login);
