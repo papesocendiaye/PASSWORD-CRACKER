d9ec4bde612eda1fd1b433047ecf6440eb0181de
papesocendiaye <133932251+papesocendiaye@users.noreply.github.com>
2025-07-22 21:56:48 +0000

    javadoc AttaqueDictionnaire

--- src/main/java/attaque/AttaqueDictionnaire.java
+++ src/main/java/attaque/AttaqueDictionnaire.java
@@ -4,11 +4,21 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import cible.Cible;
-
+/**
+ * Classe représentant une attaque par dictionnaire.
+ * Cette classe implémente l'interface Attaque et tente une attaque par dictionnaire
+ * sur une cible donnée, en utilisant les mots de passe présents dans un fichier dictionnaire.
+ */
 public class AttaqueDictionnaire implements Attaque {
 
     private static final String DICTIONARY_FILE = "../../../src/dictionnaire.txt"; 
-
+    /**
+     * Lance l'attaque par dictionnaire sur la cible spécifiée.
+     * Cette méthode lit chaque ligne du fichier dictionnaire et tente de se connecter à la cible
+     * avec chaque mot de passe. Si un mot de passe valide est trouvé, l'attaque s'arrête.
+     * 
+     * @param cible La cible sur laquelle l'attaque doit être lancée.
+     */
     @Override
     public void attaquer(Cible cible) {
         System.out.println("Démarrage de l'attaque par dictionnaire...");
@@ -18,7 +28,7 @@ public class AttaqueDictionnaire implements Attaque {
                 System.out.println("Tentative de mot de passe: " + line);
                 if (cible.tenterConnexion(line)) {
                     System.out.println("Mot de passe trouvé: " + line);
-                    return; // Mot de passe trouvé, on arrête
+                    return; 
                 }
             }
             System.out.println("Echec de l'attaque par dictionnaire ! Yorougnne mot de passe biii !!! ");
