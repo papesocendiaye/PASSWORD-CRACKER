f319c870a9411b7477ce893c535cb59fa903a93e
papesocendiaye <133932251+papesocendiaye@users.noreply.github.com>
2025-07-22 21:58:29 +0000

    javadoc  interface Attaque

--- src/main/java/attaque/Attaque.java
+++ src/main/java/attaque/Attaque.java
@@ -1,7 +1,18 @@
 package attaque;
-
 import cible.Cible;
 
+/**
+ * Interface représentant une attaque.
+ * Cette interface définit une méthode `attaquer` que toutes les attaques doivent implémenter.
+ * Elle permet d'exécuter une attaque sur une cible donnée.
+ */
 public interface Attaque {
+    /**
+     * Lance l'attaque sur la cible spécifiée.
+     * Chaque classe implémentant cette interface devra fournir une implémentation
+     * de cette méthode pour effectuer l'attaque de manière spécifique.
+     * 
+     * @param cible La cible sur laquelle l'attaque doit être lancée.
+     */
     void attaquer(Cible cible);
 }
