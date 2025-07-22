d8171b87972b9b789907062d7d14398829eabf9a
papesocendiaye <133932251+papesocendiaye@users.noreply.github.com>
2025-07-22 22:06:34 +0000

    javaadoc Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
 import factory.Fabrique;
 import attaque.Attaque;
 import cible.Cible;
-
+/**
+ * Classe principale qui exécute l'attaque spécifiée sur la cible donnée.
+ * La classe prend des arguments en ligne de commande pour spécifier le type d'attaque,
+ * le type de cible et le login à utiliser.
+ */
 public class Main {
-
+    /**
+     * Point d'entrée du programme.
+     * Cette méthode crée une attaque et une cible en fonction des arguments passés en ligne de commande,
+     * puis lance l'attaque sur la cible.
+     * 
+     * @param args Les arguments passés en ligne de commande.
+     *             - args[0] : Option (non utilisée dans ce code)
+     *             - args[1] : Type d'attaque (brute_force ou dictionnaire)
+     *             - args[2] : Option (non utilisée dans ce code)
+     *             - args[3] : Type de cible (local ou online)
+     *             - args[4] : Option (non utilisée dans ce code)
+     *             - args[5] : Login utilisé pour la cible
+     */
     public static void main(String[] args) {
         if (args.length < 6) {
             System.out.println("Usage: java Main --type <brute_force|dictionnaire> --cible <local|online> --login <login>");
